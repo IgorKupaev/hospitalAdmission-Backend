e1a7482b7302539b6f2de7ddc54e0697009f00af
user <kupaev.itrum@gmail.com>
2023-02-22 10:02:32 +0300

    fixed bug

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const apiRoutes = require("./src/routes/admission");
 const loadApp = async () => {
   try {
     mongoose.set('strictQuery', false);
+    app.use(function (req, res, next) {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', '*');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+      next();
+    });
     app.use(express.json());
     app.use('/', apiRoutes);
     app.use(cors());
